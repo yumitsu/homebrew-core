5b973c24cb05c4529b59d67c70c9ac2afbfe2454
Alex Zinchenko <yumitsu@users.noreply.github.com>
2019-07-07 15:36:19 +0300

    Add dte v1.8.2

---
+++ Formula/dte.rb
@@ -0,0 +1,15 @@
+class Dte < Formula
+  desc "Small and configurable console text editor"
+  homepage "https://github.com/craigbarnes/dte"
+  url "https://github.com/craigbarnes/dte/releases/download/v1.8.2/dte-1.8.2.tar.gz"
+  sha256 "778786c0b2588f0d9a651ebfde939885a5579745dae8f5d9adc480f4895d6c04"
+
+  def install
+    system "make", "-j#{ENV.make_jobs}"
+    system "make", "prefix=#{prefix}", "install"
+  end
+
+  test do
+    assert_equal "20", shell_output("#{bin}/dte -b compilers/gcc | wc -l")
+  end
+end
